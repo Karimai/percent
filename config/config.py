cc65d4d6c972b95db66da4f064789c6bc471f338
Karim Moradi <kmoradi.ai@gmail.com>
2023-08-02 20:46:48 +0200

    connect with pg8000

--- config/config.py
+++ config/config.py
@@ -31,7 +31,8 @@ if not DATABASE_URL:
         # f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@/{DB_NAME}?host=/cloudsql/{DB_CLOUD}"
 
         # DATABASE_URL = f"postgresql+psycopg2://postgres:postgres@/{DB_NAME}?host=/cloudsql/{DB_CLOUD}"
-        DATABASE_URL = f"postgresql+psycopg2://postgres:postgres@/percentdb?host=/cloudsql/percentpassed:us-central1:percent"
+        # DATABASE_URL = f"postgresql+psycopg2://postgres:postgres@/percentdb?host=/cloudsql/percentpassed:us-central1:percent"
+        DATABASE_URL = f"postgresql+pg8000://postgres:postgres@/percent:5432/percentdb"
         # sqlalchemy.exc.OperationalError: (psycopg2.OperationalError) connection to server on socket
         # "/cloudsql/percentpassed:us-central1:percent/.s.PGSQL.5432" failed: No such file or directory
     else:
