fa937465fe8da98fe13ece09c52660867abc5244
Karim Moradi <kmoradi.ai@gmail.com>
2023-08-04 20:26:17 +0200

    Add city (#5)
    * use countries.csv
    
    * add city

--- static/js/utility.js
+++ static/js/utility.js
@@ -24,19 +24,71 @@ $(document).ready(function () {
         }
     });
 
-    // Fetch countries and populate the select element
-    const selectDrop = document.querySelector('#country');
-    fetch('https://restcountries.com/v2/all') // Use the correct API endpoint
-      .then(res => res.json())
-      .then(data => {
-        let output = '<option value="" disabled selected>Select a country</option>';
-        data.forEach(country => {
-          output += `<option value="${country.name}" ${selectedCountry === country.name ? 'selected' : ''}>${country.name}</option>`;
-        })
-        selectDrop.innerHTML = output;
-      })
-      .catch(err => {
-        console.log(err);
-      });
-
 });
+
+// Assuming your CSV file is named "country.csv" and is in the same directory as this HTML file.
+const csvFilePath = '../static/countries.csv';
+async function fetchCSVFile(filePath) {
+    try {
+        const response = await fetch(filePath);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch ${filePath}. Status: ${response.status}`);
+        }
+        return await response.text();
+    } catch (error) {
+        console.error('Error fetching CSV file:', error);
+        return null;
+    }
+}
+
+const selectElement = document.getElementById('country');
+async function populateCountryDropdown() {
+    const csvData = await fetchCSVFile(csvFilePath);
+
+    if (!csvData) {
+        selectElement.innerHTML = '<option disabled selected>Select a country</option>';
+        return;
+    }
+
+    const countries = csvData.split('\n').map(country => country.trim());
+
+    selectElement.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
+}
+
+function fillCitiesForCountry(country_code) {
+
+    const citySelect = document.getElementById('city');
+    citySelect.innerHTML = '<option value="">Select City</option>';
+
+    const overpassApiUrl = 'https://overpass-api.de/api/interpreter';
+    const query = `[out:json];
+                area["ISO3166-1"="${country_code}"];
+                node["place"="city"](area);
+                out;`;
+
+    axios.post(overpassApiUrl, query)
+        .then(response => {
+            const cities = response.data.elements;
+
+            cities.forEach(city => {
+                const cityName = city.tags['name:en'] || city.tags.name;
+                const option = document.createElement('option');
+                option.value = cityName;
+                option.textContent = cityName;
+                citySelect.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching cities:', error);
+        });
+
+}
+
+selectElement.addEventListener('change', function () {
+    const selectedCountry = selectElement.value;
+    country_code = selectedCountry.split(",")[1];
+    fillCitiesForCountry(country_code);
+});
+
+document.addEventListener('DOMContentLoaded', populateCountryDropdown);
+
