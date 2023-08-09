99dd8f7051ca76e7033422fc1afed417b0827f89
Karim Moradi <kmoradi.ai@gmail.com>
2023-08-09 19:26:27 +0200

    limit date selection

--- static/js/utility.js
+++ static/js/utility.js
@@ -1,16 +1,4 @@
 $(document).ready(function () {
-    var dateInputs = ['start-date', 'end-date'];
-    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
-
-    for (var i = 0; i < dateInputs.length; i++) {
-        var input = $('input[name="' + dateInputs[i] + '"]');
-        input.datepicker({
-            format: 'yyyy-mm-dd',
-            container: container,
-            todayHighlight: true,
-            autoclose: true,
-        });
-    }
 
     $(window).on('resize', function () {
         if ($(this).width() > 800) {
