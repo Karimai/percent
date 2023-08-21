2e192bdabef98cd82260d3bfdae2308ec7da4a0e
Karim Moradi <kmoradi.ai@gmail.com>
2023-08-21 11:15:39 +0200

    add test for auth endpoint

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -8,3 +8,19 @@ client = TestClient(app)
 def test_read_index():
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_auth_endpoint():
+    # Test user data
+    test_user = {"username": "testuser123", "password": "Testuser123"}
+
+    # Send a POST request to the /auth/ endpoint
+    response = client.post("/login/auth/", data=test_user)
+
+    # Check if the response status code is 200 OK
+    assert response.status_code == 200
+
+    # Check if the response contains the expected keys
+    assert "access_token" in response.json()
+    response_data = response.json()
+    assert response_data["token_type"] == "bearer"
